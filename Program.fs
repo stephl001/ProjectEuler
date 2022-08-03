9adeeab9310287c23a934c721c9960bbffee931b
Stéphane Larocque <steflar@github.com>
2022-08-03 15:46:46 -0400

    Display all answers

--- Program.fs
+++ Program.fs
@@ -1,1 +1,7 @@
-﻿printfn "%A" Problem1.response
+﻿let solvedProblems = [
+    (1, Problem1.response.ToString())
+    (2, Problem2.response.ToString())
+]
+
+solvedProblems |>
+List.iter (fun (pbNum, pbResponse) -> printfn $"Problem #{pbNum}: {pbResponse}")
